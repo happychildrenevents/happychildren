a56a11ad7132c367faa614621da8c94d3b91712e
HappyChildren <carolinalopeznieto@gmail.com>
2018-06-11 12:35:41 -0500

    ACtualizacion del Shopping

---
+++ page/assets/js/shop.js
@@ -0,0 +1,75 @@
+var fadeTime = 300;
+
+var taxRate = 0.00;
+var shippingRate = 0.00;
+
+
+/* Assign actions */
+$('.product-quantity input').change( function() {
+  updateQuantity(this);
+});
+
+
+
+/* Recalculate cart */
+function recalculateCart()
+{
+  var subtotal = 0;
+
+  /* Sum up row totals */
+  $('.product').each(function () {
+    subtotal += parseFloat(parseLocalNum($(this).children('.product-line-price').text()));
+  });
+
+  /* Calculate totals */
+  var tax = subtotal * taxRate;
+  var shipping = (subtotal > 0 ? shippingRate : 0);
+  var total = subtotal + tax + shipping;
+
+  /* Update totals display */
+  $('.totals-value').fadeOut(fadeTime, function() {
+    $('#cart-subtotal').html(subtotal.format(0, 3, '.') );
+    $('#cart-tax').html(tax.format(0, 3, '.') );
+    $('#cart-shipping').html(shipping.format(0, 3, '.') );
+    $('#cart-total').html(total.format(0, 3, '.') );
+    if(total == 0){
+      $('.checkout').fadeOut(fadeTime);
+    }else{
+      $('.checkout').fadeIn(fadeTime);
+    }
+    $('.totals-value').fadeIn(fadeTime);
+  });
+}
+
+
+/* Update quantity */
+function updateQuantity(quantityInput)
+{
+  /* Calculate line price */
+  var productRow = $(quantityInput).parent().parent();
+  var price = parseFloat(parseLocalNum(productRow.children('.product-price').text()));
+  var quantity = $(quantityInput).val();
+  var linePrice = price * quantity;
+
+  /* Update line price display and recalc cart totals */
+  productRow.children('.product-line-price').each(function () {
+    $(this).fadeOut(fadeTime, function() {
+      $(this).text(linePrice.format(0, 3, '.') );
+      recalculateCart();
+      $(this).fadeIn(fadeTime);
+    });
+  });
+}
+
+
+function parseLocalNum(num) {
+    return +(num.replace(".", ""));
+}
+
+
+Number.prototype.format = function(n, x, s, c) {
+    var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\D' : '$') + ')',
+        num = this.toFixed(Math.max(0, ~~n));
+
+    return (c ? num.replace('.', c) : num).replace(new RegExp(re, 'g'), '$&' + (s || ','));
+};
